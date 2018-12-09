c04d4e7f462082d55db8334f43751a4a92074498
Alex Didenko <alex.didenko753@gmail.com>
2018-12-09 20:40:16 +0300

    refact & update

--- resources/views/page/behaviors/diet.blade.php
+++ resources/views/page/behaviors/diet.blade.php
@@ -105,39 +105,25 @@
                 <div class="row justify-content-sm-center offset-top-25">
                     <div class="col-md-10">
                         <div class="tab-content text-left">
-                            @for ($i = 1; $i < 7; $i++)
-                                @php($j = $i + 1)
-                                <div class="tab-pane fade {{($i==1)?'show active':''}}" id="tabs-1-{{$i}}">
+                            @for ($weekNum = 1; $weekNum < 7; $weekNum++)
+                                @php($j = $weekNum + 1)
+                                <div class="tab-pane fade {{($weekNum==1)?'show active':''}}" id="tabs-1-{{$weekNum}}">
                                     <div class="container offset-top-65 offset-md-top-90">
                                         <h3>Vertical tabs</h3>
                                         <div class="row justify-content-sm-center offset-top-13">
                                             <div class="col-md-10">
-                                                <!-- Bootstrap tabs-->
                                                 <div class="tabs-custom tabs-vertical tabs-corporate" id="tabs-{{$j}}">
-                                                    <!-- Nav tabs-->
                                                     <ul class="nav nav-link">
-                                                        <li class="active"><a href="#tabs-{{$j}}-1" data-toggle="tab">Tab 0</a></li>
-                                                        <li><a href="#tabs-{{$j}}-2" data-toggle="tab">Tab 1</a></li>
-                                                        <li><a href="#tabs-{{$j}}-3" data-toggle="tab">Tab 2</a></li>
-                                                        <li><a href="#tabs-{{$j}}-4" data-toggle="tab">Tab 3</a></li>
+                                                        @for ($k = 1; $k <= 7; $k++)
+                                                            <li class="{{($k==1)?'active':''}}"><a href="#tabs-{{$j}}-{{$k}}" data-toggle="tab">Tab {{$k}}</a></li>
+                                                        @endfor
                                                     </ul>
                                                     <div class="tab-content text-left">
-                                                        <div class="tab-pane fade show active" id="tabs-{{$j}}-1">
-                                                            <p>Welcome to our wonderful world. We sincerely hope that each and every user entering our website will find exactly what he/she is looking for. With advanced features of activating account and new login widgets, you will definitely have a great experience of using our web page. It will tell you lots of interesting things about our company, its products and services, highly professional staff and happy customers. Our site design and navigation has been thoroughly thought out. The layout is aesthetically appealing, contains concise texts in order not to take your precious time. Text styling allows scanning the pages quickly.</p>
-                                                            <p>Site navigation is extremely intuitive and user-friendly. You will always know where you are now and will be able to skip from one page to another with a single mouse click. We use only trusted, verified content, so you can believe every word we are saying. We are always happy to greet the new visitors on our site.</p>
-                                                        </div>
-                                                        <div class="tab-pane fade" id="tabs-{{$j}}-2">
-                                                            <p>The layout is aesthetically appealing, contains concise texts in order not to take your precious time. Text styling allows scanning the pages quickly. Site navigation is extremely intuitive and user-friendly. You will always know where you are now and will be able to skip from one page to another with a single mouse click.</p>
-                                                            <p>We use only trusted, verified content, so you can believe every word we are saying. We are always happy to greet the new visitors on our site. Our blog and social media accounts are available to encourage communication and connection between clients and personnel and tell you more about us in the informal environments where we can have a dialogue instead of just a narrative like that.</p>
-                                                        </div>
-                                                        <div class="tab-pane fade" id="tabs-{{$j}}-3">
-                                                            <p>We sincerely hope that each and every user entering our website will find exactly what he/she is looking for. With advanced features of activating account and new login widgets, you will definitely have a great experience of using our web page. It will tell you lots of interesting things about our company, its products and services, highly professional staff and happy customers.</p>
-                                                            <p>Our site design and navigation has been thoroughly thought out. The layout is aesthetically appealing, contains concise texts in order not to take your precious time. Text styling allows scanning the pages quickly. Site navigation is extremely intuitive and user-friendly. You will always know where you are now and will be able to skip from one page to another with a single mouse click.</p>
-                                                        </div>
-                                                        <div class="tab-pane fade" id="tabs-{{$j}}-4">
-                                                            <p>Our site design and navigation has been thoroughly thought out. The layout is aesthetically appealing, contains concise texts in order not to take your precious time. Text styling allows scanning the pages quickly. Site navigation is extremely intuitive and user-friendly.</p>
-                                                            <p>You will always know where you are now and will be able to skip from one page to another with a single mouse click. We use only trusted, verified content, so you can believe every word we are saying. We are always happy to greet the new visitors on our site. With advanced features of activating account and new login widgets, you will definitely have a great experience of using our web page. It will tell you lots of interesting things about our company, its products and services, highly professional staff and happy customers.</p>
-                                                        </div>
+                                                        @for ($k = 1; $k <= 7; $k++)
+                                                            <div class="tab-pane fade {{($k==1)?'show active':''}}" id="tabs-{{$j}}-{{$k}}">
+                                                                <p>Неделя {{$weekNum}}, день недели {{$k}}</p>
+                                                            </div>
+                                                        @endfor
                                                     </div>
                                                 </div>
                                             </div>
