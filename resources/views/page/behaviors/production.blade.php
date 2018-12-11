6612709d57d8b9412bdf9f14a0d621701ec1bae3
Alex Didenko <alex.didenko753@gmail.com>
2018-12-11 18:53:24 +0300

    added production template

---
+++ resources/views/page/behaviors/production.blade.php
@@ -0,0 +1,268 @@
+@extends('layouts.app')
+
+@section('content')
+
+  <main class="page-content">
+    <!-- Latest news-->
+    <section class="section-95">
+      <div class="container">
+        <h2>Latest news</h2>
+        <div class="row no-gutters justify-content-sm-center offset-top-40 shadow-drop-sm">
+          <div class="col-md-6 col-lg-4"><img class="img-responsive img-fullwidth img-fullheight center-block"
+                                              src="images/pages/events-01-390x289.jpg" width="390" height="289" alt="">
+          </div>
+          <div class="col-md-6 col-lg-4">
+            <!-- Box-->
+            <div class="box-xs inset-xl-left-35 inset-xl-right-35"><span
+                  class="icon fl-great-icon-set-ico fl-great-icon-set-pencil107 text-primary"></span>
+              <p class="text-big font-weight-bold"><a class="link-decoration-none" href="blog-post.html">Seasonal
+                  diet</a></p>
+              <p>Ever wondered why you crave certain foods? One reason is the change in seasons. Our bodies seek foods
+                that balance out the elements of the season…</p>
+              <p class="text-big font-weight-bold offset-top-10">. . .</p>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-4"><img class="img-responsive img-fullwidth img-fullheight center-block"
+                                              src="images/pages/events-02-390x289.jpg" width="390" height="289" alt="">
+          </div>
+          <div class="col-md-6 col-lg-4">
+            <!-- Box-->
+            <div class="box-xs inset-xl-left-35 inset-xl-right-35"><span
+                  class="icon fl-great-icon-set-ico fl-great-icon-set-video172 text-primary"></span>
+              <p class="text-big font-weight-bold"><a class="link-decoration-none" href="blog-post.html">Street
+                  workout</a></p>
+              <p>Daily movement, in some form, will make you feel better each and every day. Your body craves moment. It
+                was built to move. That means, we need to find...</p>
+              <p class="text-big font-weight-bold offset-top-10">. . .</p>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-4"><img class="img-responsive img-fullwidth img-fullheight center-block"
+                                              src="images/pages/events-03-390x289.jpg" width="390" height="289" alt="">
+          </div>
+          <div class="col-md-6 col-lg-4">
+            <!-- Box-->
+            <div class="box-xs inset-xl-left-35 inset-xl-right-35"><span
+                  class="icon fl-great-icon-set-ico fl-great-icon-set-pencil107 text-primary"></span>
+              <p class="text-big font-weight-bold"><a class="link-decoration-none" href="blog-post.html">How to eat</a>
+              </p>
+              <p>We tend to be so conscious about WHAT to eat - fruits and vegetables, nuts, seeds, quality fats and
+                proteins, sprouts, herbs etc. But HOW to eat is just as important, if not more. </p>
+              <p class="text-big font-weight-bold offset-top-10">. . .</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- 25 Simple self care actions for a healthier life-->
+    <!-- Parallax-->
+    <section class="section section-height-800 parallax-container context-dark"
+             data-parallax-img="images/backgrounds/background-11-1920x900.jpg">
+      <div class="parallax-content">
+        <div class="bg-overlay-green-leaf">
+          <div class="container section-110 section-md-170">
+            <div class="row justify-content-sm-center">
+              <div class="col-sm-10">
+                <h1>25 Simple self care actions for a healthier life</h1>
+                <p class="text-big text-uppercase text-spacing-100">I’ve had a particularly rough month. I’ve been
+                  working tons and getting ready for a very big move.</p><a
+                    class="btn btn-white-outline offset-top-20 offset-md-top-30" href="group-coaching.html">Start
+                  training program</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Featured events-->
+    <section class="section-95">
+      <div class="container">
+        <h2>Featured events</h2>
+        <!-- Owl Carousel-->
+        <div class="owl-carousel owl-nav-modern owl-carousel-inset-bottom-20 owl-carousel-navs-primary offset-top-40"
+             data-autoplay="true" data-items="1" data-md-items="2" data-stage-padding="5" data-loop="true"
+             data-margin="30" data-mouse-drag="false" data-dots="true" data-nav="true">
+          <div class="owl-item">
+            <!-- Post Box-->
+            <article class="post-box">
+              <div class="post-box-img-wrap"><img src="images/blog/post-01-571x234.jpg" width="571" height="234" alt="">
+              </div>
+              <div class="post-box-caption bg-default"><span
+                    class="icon icon-circle fl-great-icon-set-ico fl-great-icon-set-pencil107 bg-default"></span>
+                <div class="post-meta font-weight-bold">September, 12, 2018</div>
+                <p class="post-box-title text-big font-weight-bold text-uppercase text-spacing-100 offset-top-5"><a
+                      class="link-decoration-none" href="blog-post.html">Seasonal diets</a></p>
+                <div class="post-box-caption-hidden">
+                  <p>Ever wondered why you crave certain foods? One reason is the change in seasons. Our bodies seek
+                    foods that balance out the elements of the season…</p>
+                </div>
+              </div>
+            </article>
+          </div>
+          <div class="owl-item">
+            <!-- Post Box-->
+            <article class="post-box">
+              <div class="post-box-img-wrap"><img src="images/blog/post-02-571x234.jpg" width="571" height="234" alt="">
+              </div>
+              <div class="post-box-caption bg-default"><span
+                    class="icon icon-circle fl-great-icon-set-ico fl-great-icon-set-video172 bg-default"></span>
+                <div class="post-meta font-weight-bold">September, 12, 2018</div>
+                <p class="post-box-title text-big font-weight-bold text-uppercase text-spacing-100 offset-top-5"><a
+                      class="link-decoration-none" href="blog-post.html">How to eat</a></p>
+                <div class="post-box-caption-hidden">
+                  <p>We tend to be so conscious about WHAT to eat - fruits and vegetables, nuts, seeds, quality fats and
+                    proteins, sprouts, herbs etc. But HOW to eat is just as important, if not more. </p>
+                </div>
+              </div>
+            </article>
+          </div>
+          <div class="owl-item">
+            <!-- Post Box-->
+            <article class="post-box">
+              <div class="post-box-img-wrap"><img src="images/blog/post-01-571x234.jpg" width="571" height="234" alt="">
+              </div>
+              <div class="post-box-caption bg-default"><span
+                    class="icon icon-circle fl-great-icon-set-ico fl-great-icon-set-pencil107 bg-default"></span>
+                <div class="post-meta font-weight-bold">September, 12, 2018</div>
+                <p class="post-box-title text-big font-weight-bold text-uppercase text-spacing-100 offset-top-5"><a
+                      class="link-decoration-none" href="blog-post.html">Seasonal diets</a></p>
+                <div class="post-box-caption-hidden">
+                  <p>Ever wondered why you crave certain foods? One reason is the change in seasons. Our bodies seek
+                    foods that balance out the elements of the season…</p>
+                </div>
+              </div>
+            </article>
+          </div>
+          <div class="owl-item">
+            <!-- Post Box-->
+            <article class="post-box">
+              <div class="post-box-img-wrap"><img src="images/blog/post-02-571x234.jpg" width="571" height="234" alt="">
+              </div>
+              <div class="post-box-caption bg-default"><span
+                    class="icon icon-circle fl-great-icon-set-ico fl-great-icon-set-video172 bg-default"></span>
+                <div class="post-meta font-weight-bold">September, 12, 2018</div>
+                <p class="post-box-title text-big font-weight-bold text-uppercase text-spacing-100 offset-top-5"><a
+                      class="link-decoration-none" href="blog-post.html">How to eat</a></p>
+                <div class="post-box-caption-hidden">
+                  <p>We tend to be so conscious about WHAT to eat - fruits and vegetables, nuts, seeds, quality fats and
+                    proteins, sprouts, herbs etc. But HOW to eat is just as important, if not more. </p>
+                </div>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Contact Us-->
+    <section class="section-95 section-md-bottom-45 bg-gray-lighter">
+      <div class="container">
+        <div class="row justify-content-sm-center justify-content-lg-start text-lg-left">
+          <div class="col-md-10 col-lg-3">
+            <h6>Address</h6>
+            <!-- Contact Info-->
+            <address class="contact-info p offset-top-20">
+              <p>4578 Marmora St, <br> Chicago D04 89GR.</p>
+              <dl>
+                <dt>Telephone:</dt>
+                <dd><a class="text-gray-light link-decoration-none text-hover-primary" href="tel:#">1-800-1234-567</a>
+                </dd>
+              </dl>
+              <dl class="offset-top-10">
+                <dt>E-mail:</dt>
+                <dd><a class="link-decoration-none" href="mailto:#">mail@demolink.org</a></dd>
+              </dl>
+            </address>
+          </div>
+          <div class="col-md-10 col-lg-5 offset-top-40 offset-md-top-0">
+            <h6>Get in touch</h6>
+            <!-- RD Mailform-->
+            <form class="rd-mailform text-left offset-top-20" data-form-output="form-output-global"
+                  data-form-type="contact" method="post" action="bat/rd-mailform.php">
+              <div class="row justify-content-sm-center">
+                <div class="col-sm-6 inset-sm-right-7">
+                  <div class="form-wrap">
+                    <label class="form-label" for="contact-us-first-name">First name</label>
+                    <input class="form-input" id="contact-us-first-name" type="text" name="name"
+                           data-constraints="@Required">
+                  </div>
+                </div>
+                <div class="col-sm-6 offset-top-10 offset-xs-top-0 inset-sm-left-7">
+                  <div class="form-wrap">
+                    <label class="form-label" for="contact-us-last-name">Last name</label>
+                    <input class="form-input" id="contact-us-last-name" type="text" name="name"
+                           data-constraints="@Required">
+                  </div>
+                </div>
+                <div class="col-sm-6 offset-top-10 inset-sm-right-7">
+                  <div class="form-wrap">
+                    <label class="form-label" for="contact-us-email">Email</label>
+                    <input class="form-input" id="contact-us-email" type="email" name="email"
+                           data-constraints="@Email @Required">
+                  </div>
+                </div>
+                <div class="col-sm-6 offset-top-10 inset-sm-left-7">
+                  <div class="form-wrap">
+                    <label class="form-label" for="contact-us-phone">Phone</label>
+                    <input class="form-input" id="contact-us-phone" type="text" name="phone"
+                           data-constraints="@Numeric @Required">
+                  </div>
+                </div>
+                <div class="col-sm-12 offset-top-10 text-center text-xl-left">
+                  <div class="form-wrap">
+                    <label class="form-label" for="contact-us-message">Message</label>
+                    <textarea class="form-input" id="contact-us-message" name="message"
+                              data-constraints="@Required"></textarea>
+                  </div>
+                </div>
+              </div>
+              <div class="text-center text-lg-right offset-top-10">
+                <button class="btn btn-primary" type="submit">Send a message</button>
+              </div>
+            </form>
+          </div>
+          <div class="col-sm-10 col-lg-3 offset-lg-1 offset-top-40 offset-md-top-0">
+            <h6>Flickr</h6>
+            <div class="group-xs flickr widget-flickrfeed offset-top-20" data-lightgallery="group"
+                 data-flickr-tags="tm61252"><a class="flickr-item" data-lightgallery="item"
+                                               href="http://farm6.staticflickr.com/5802/29943773064_35b614b909_c.jpg"
+                                               data-image_c="href" data-size="1000x780" data-type="flickr-item"><img
+                    width="87" height="83" data-title="alt"
+                    src="http://farm6.staticflickr.com/5802/29943773064_35b614b909_q.jpg" alt="Movies"
+                    data-image_q="src"></a><a class="flickr-item" data-lightgallery="item"
+                                              href="http://farm6.staticflickr.com/5681/29943773084_315b1fbcd5_c.jpg"
+                                              data-image_c="href" data-size="1000x780" data-type="flickr-item"><img
+                    width="87" height="83" data-title="alt"
+                    src="http://farm6.staticflickr.com/5681/29943773084_315b1fbcd5_q.jpg"
+                    alt="Sexy and fashionable couple in sunglasses. Vogue" data-image_q="src"></a><a class="flickr-item"
+                                                                                                     data-lightgallery="item"
+                                                                                                     href="http://farm6.staticflickr.com/5623/29943772684_2273107e5b_c.jpg"
+                                                                                                     data-image_c="href"
+                                                                                                     data-size="1000x780"
+                                                                                                     data-type="flickr-item"><img
+                    width="87" height="83" data-title="alt"
+                    src="http://farm6.staticflickr.com/5623/29943772684_2273107e5b_q.jpg" alt="5"
+                    data-image_q="src"></a><a class="flickr-item" data-lightgallery="item"
+                                              href="http://farm6.staticflickr.com/5334/29943772954_5df42932e2_c.jpg"
+                                              data-image_c="href" data-size="1000x780" data-type="flickr-item"><img
+                    width="87" height="83" data-title="alt"
+                    src="http://farm6.staticflickr.com/5334/29943772954_5df42932e2_q.jpg" alt="traditional breakfast"
+                    data-image_q="src"></a><a class="flickr-item" data-lightgallery="item"
+                                              href="http://farm6.staticflickr.com/5334/29943772954_5df42932e2_c.jpg"
+                                              data-image_c="href" data-size="1000x780" data-type="flickr-item"><img
+                    width="87" height="83" data-title="alt"
+                    src="http://farm6.staticflickr.com/5334/29943772954_5df42932e2_q.jpg" alt="traditional breakfast"
+                    data-image_q="src"></a><a class="flickr-item" data-lightgallery="item"
+                                              href="http://farm6.staticflickr.com/5334/29943772954_5df42932e2_c.jpg"
+                                              data-image_c="href" data-size="1000x780" data-type="flickr-item"><img
+                    width="87" height="83" data-title="alt"
+                    src="http://farm6.staticflickr.com/5334/29943772954_5df42932e2_q.jpg" alt="traditional breakfast"
+                    data-image_q="src"></a></div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+
+
+@endsection
+
+
