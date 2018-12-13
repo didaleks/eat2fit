7b4cdbcc507d176bf9ca172ef0b96413c6b15f3b
Alex Didenko <alex.didenko753@gmail.com>
2018-12-13 11:37:03 +0300

    accordion

--- resources/views/page/behaviors/about.blade.php
+++ resources/views/page/behaviors/about.blade.php
@@ -74,43 +74,99 @@
 
     {{-- Блок «Как готовим и храним продукты» --}}
     <section class="section-95 about__how-cook">
-      <div class="container">
-        <h2>Блок «Как готовим и храним продукты»</h2>
-        <div class="row justify-content-sm-center offset-top-40">
-          <div class="col-md-10 col-lg-6"><img class="img-responsive center-block"
-                                               src="images/pages/health-coaching-philosophy-02-570x350.jpg" width="570"
-                                               height="350" alt=""></div>
-          <div class="col-md-10 col-lg-6 text-left offset-top-40 offset-md-top-0">
-            <p class="text-big font-weight-bold text-primary">Why health coaching</p>
-            <p>I am thrilled you are here because it means that like me, you are a health conscious individual. Whether
-              you already consider yourself healthy or you are just awakening to the potential of a healthy lifestyle,
-              this site is for you.</p>
-            <!-- List Marked-->
-            <ul class="list list-marked font-weight-bold text-primary offset-top-13">
-              <li>You struggle with emotional or binge eating</li>
-              <li>You have unrestful sleep and wake up tired</li>
-              <li>You feel stressed and anxious</li>
-              <li>You want to learn about health and wellness</li>
-              <li>You realise that health should be one of your top priorities</li>
-            </ul>
-            <a class="btn btn-primary offset-top-25 offset-md-top-40" href="group-coaching.html">Read more</a>
+      <h3>Блок «Как готовим и храним продукты»</h3>
+      <div class="container d-flex justify-content-center offset-top-65 offset-md-top-90">
+        <div class="col-md-10 col-lg-6">
+          <div class="about__how-cook__img collapse  panel-collapse-one" id="collapseOneImg">
+            <span>1</span>
+            <img class="img-responsive center-block" src="images/pages/praise-01-269x278.jpg"
+                 width="269" height="278" alt="">
+          </div>
+          <div class="about__how-cook__img collapse  panel-collapse-two" id="collapseTwoImg">
+            <span>2</span>
+            <img class="img-responsive center-block" src="images/pages/praise-01-269x278.jpg"
+                 width="269" height="278" alt="">
+          </div>
+          <div class="about__how-cook__img collapse  panel-collapse-three" id="collapseThreeImg">
+            <span>3</span>
+            <img class="img-responsive center-block" src="images/pages/praise-01-269x278.jpg"
+                 width="269" height="278" alt="">
+          </div>
+          <div class="about__how-cook__img collapse  panel-collapse-four" id="collapseFourImg">
+            <span>4</span>
+            <img class="img-responsive center-block" src="images/pages/praise-01-269x278.jpg"
+                 width="269" height="278" alt="">
+          </div>
+          <div class="about__how-cook__img collapse  panel-collapse-five" id="collapseFiveImg">
+            <span>5</span>
+            <img class="img-responsive center-block" src="images/pages/praise-01-269x278.jpg"
+                 width="269" height="278" alt="">
+          </div>
+        </div>
+
+        <div class="col-md-10 col-lg-6 offset-top-25">
+          <!-- Accordion-->
+          <div class="panel-group text-left" id="accordion" role="tablist" aria-multiselectable="true">
+            <div class="panel panel-custom panel-default">
+              <div class="panel-heading" id="headingOne" role="tab">
+                <p class="panel-title"><a class="d-block collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="false" aria-controls="collapseOne">How can I change something in my order?</a></p>
+              </div>
+              <div class="panel-collapse collapse" id="collapseOne" role="tabpanel" aria-labelledby="headingOne" style="">
+                <div class="panel-body">If you need to change something in your order, please contact us immediately. We usually process orders within 2-4 hours, and once we have processed your order, we will be unable to make any changes.</div>
+              </div>
+            </div>
+            <div class="panel panel-custom panel-default">
+              <div class="panel-heading" id="headingTwo" role="tab">
+                <p class="panel-title"><a class="d-block collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">How can I pay for my order?</a></p>
+              </div>
+              <div class="panel-collapse collapse" id="collapseTwo" role="tabpanel" aria-labelledby="headingTwo" style="">
+                <div class="panel-body">We accept Visa, MasterCard, and American Express credit and debit cards for your convenience.</div>
+              </div>
+            </div>
+            <div class="panel panel-custom panel-default">
+              <div class="panel-heading" id="headingThree" role="tab">
+                <p class="panel-title"><a class="d-block collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">How long will my order take to be delivered?</a></p>
+              </div>
+              <div class="panel-collapse collapse" id="collapseThree" role="tabpanel" aria-labelledby="headingOne" style="">
+                <div class="panel-body">Delivery times will depend on your location. Once payment is confirmed your order will be packaged. Delivery can be expected within 10 business days.</div>
+              </div>
+            </div>
+            <div class="panel panel-custom panel-default">
+              <div class="panel-heading" id="headingFour" role="tab">
+                <p class="panel-title"><a class="d-block collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseFour" aria-expanded="false" aria-controls="collapseFour">Can I return an item?</a></p>
+              </div>
+              <div class="panel-collapse collapse" id="collapseFour" role="tabpanel" aria-labelledby="headingOne" style="">
+                <div class="panel-body">Please contact our administrators for more information on returning products purchased on our website.</div>
+              </div>
+            </div>
+            <div class="panel panel-custom panel-default">
+              <div class="panel-heading" id="headingFive" role="tab">
+                <p class="panel-title"><a class="d-block" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseFive" aria-expanded="true" aria-controls="collapseFive">Can I track my order?</a></p>
+              </div>
+              <div class="panel-collapse in collapse show" id="collapseFive" role="tabpanel" aria-labelledby="headingThree" style="">
+                <div class="panel-body">Yes, you can! After placing your order you will receive an order confirmation via email. Each order starts production 24 hours after your order is placed. Within 72 hours of you placing your order, you will receive an expected delivery date. When the order ships, you will receive another email with the tracking number and a link to trace the order online with the carrier.</div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
+
     </section>
 
-    {{-- Блок "Лицензии" --}}
+    {{-- Блок "Лицензии"
     <section class="section-95">
       <div class="container">
-        <h2>Success stories</h2>
+        <h2>Наши лицензии</h2>
         <div class="row justify-content-sm-center offset-top-40">
           <div class="col-md-10 col-lg-8 col-xl-6">
             <div class="row justify-content-sm-center">
-              <div class="col-sm-6"><img class="img-responsive center-block" src="images/pages/praise-01-269x278.jpg"
-                                         width="269" height="278" alt=""></div>
-              <div class="col-sm-6 offset-top-30 offset-xs-top-0"><img class="img-responsive center-block"
-                                                                       src="images/pages/praise-02-269x278.jpg"
-                                                                       width="269" height="278" alt=""></div>
+              <div class="col-sm-6">
+                <img class="img-responsive center-block" src="images/pages/praise-01-269x278.jpg"
+                     width="269" height="278" alt="">
+              </div>
+              <div class="col-sm-6 offset-top-30 offset-xs-top-0">
+                <img class="img-responsive center-block" src="images/pages/praise-02-269x278.jpg"
+                     width="269" height="278" alt=""></div>
             </div>
           </div>
           <div class="col-md-10 col-lg-8 col-xl-6 offset-top-40 offset-lg-top-0 text-left">
@@ -203,6 +259,8 @@
         </div>
       </div>
     </section>
+     --}}
+
 
     {{-- Задать вопрос диетологу --}}
     <section class="section-bottom-95 about__paralax-question">
@@ -262,7 +320,7 @@
     {{-- Блок Команда --}}
     <section class="section-95 about__team">
       <div class="container">
-        <h2>News &amp; articles</h2>
+        <h2>Наша команда</h2>
         <div class="row justify-content-sm-center offset-top-40">
           <div class="col-md-8 col-lg-4">
             <!-- Post Box Hover-->
@@ -352,7 +410,7 @@
         </div>
         <div class="owl-carousel owl-carousel-centered offset-top-50"
              data-autoplay="true" data-items="2" data-sm-items="5"
-             data-xl-items="10" data-stage-padding="5" data-loop="true"
+             data-xl-items="5" data-stage-padding="5" data-loop="true"
              data-margin="30" data-mouse-drag="false"
              data-dots="true">
           <a href="#">
