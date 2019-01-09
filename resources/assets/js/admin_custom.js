d2971f1325d10f1dadc075c8a8c165a8ae8fa0a9
Alex Didenko <alex.didenko753@gmail.com>
2019-01-09 15:23:56 +0300

    dishes search in select

--- resources/assets/js/admin_custom.js
+++ resources/assets/js/admin_custom.js
@@ -1,4 +1,5 @@
 require('./jquery.multi-select');
+require('./jquery.quicksearch');
 
 var select = $('form #dishes-select');
 var hidden_input = $('form .dishes-select-ids');
@@ -8,8 +9,39 @@ function processDishesSelect(select, hidden_input) {
     var select_data = select.data('ids');
     select.multiSelect({
         keepOrder: true,
-        afterInit: function () {
+        selectableHeader: "<p class='font-weight-bold'>Список всех блюд</p><input type='text' class='search-input form-control mb-2' autocomplete='off' placeholder='Введите название'>",
+        selectionHeader: "<p class='font-weight-bold'>Меню приёма пищи</p><input type='text' class='search-input form-control mb-2' autocomplete='off' placeholder='Введите название'>",
+        afterInit: function(ms){
+            var that = this,
+                $selectableSearch = that.$selectableUl.prev(),
+                $selectionSearch = that.$selectionUl.prev(),
+                selectableSearchString = '#'+that.$container.attr('id')+' .ms-elem-selectable:not(.ms-selected)',
+                selectionSearchString = '#'+that.$container.attr('id')+' .ms-elem-selection.ms-selected';
+
+            that.qs1 = $selectableSearch.quicksearch(selectableSearchString)
+                .on('keydown', function(e){
+                    if (e.which === 40){
+                        that.$selectableUl.focus();
+                        return false;
+                    }
+                });
+
+            that.qs2 = $selectionSearch.quicksearch(selectionSearchString)
+                .on('keydown', function(e){
+                    if (e.which == 40){
+                        that.$selectionUl.focus();
+                        return false;
+                    }
+                });
             select_data = hidden_input.val().split(',');
+        },
+        afterSelect: function(){
+            this.qs1.cache();
+            this.qs2.cache();
+        },
+        afterDeselect: function(){
+            this.qs1.cache();
+            this.qs2.cache();
         },
         afterSelect: function (values) {
             select_data.push(parseInt(values[0]));
