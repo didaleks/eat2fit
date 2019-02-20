6ebe2ed0888ec7cbe9019ef98202d4960967562c
Alex Didenko <alex.didenko753@gmail.com>
2019-02-20 18:21:32 +0300

    fix customer mail

--- resources/views/emails/customer_mail.blade.php
+++ resources/views/emails/customer_mail.blade.php
@@ -3,5 +3,5 @@ $settings = new \LaravelAdmin\Models\Settings()
 ?>
 <base href="{{config('app.url')}}">
 <p>Ваш заказ №{{$model->id}} принят на обработку. Вам перезвонят в ближайшее время.</p>
-<p>Оплата доставки произодится отдельно на странице на странице <a href="{{config('app.url')}}/free-pay">{{config('app.url')}}/free-pay</a></p>
+<p>Оплата доставки произодится отдельно на странице <a href="{{config('app.url')}}/free-pay">{{config('app.url')}}/free-pay</a></p>
 <p>По любым вопросам обращайтесь по телефону: {{$settings->phone}}</p>
